aa7bbb0f1f9d07ae958285199f1f7ae99f258429
Maksym2105 <maxdetrai02@gmail.com>
2024-12-04 11:47:56 +0200

    Participant Solution

---
+++ src/app/models/Robot.java
@@ -0,0 +1,44 @@
+package app.models;
+
+import app.service.Participant;
+
+public class Robot implements Participant {
+
+    private String name;
+    private int maxRunDistance;
+    private int maxJumpDistance;
+
+
+    public Robot(String name, int maxRunDistance, int maxJumpDistance) {
+        this.name = name;
+        this.maxRunDistance = maxRunDistance;
+        this.maxJumpDistance = maxJumpDistance;
+    }
+
+    @Override
+    public boolean run(int distance) {
+        if(distance <= maxRunDistance){
+            System.out.println(name + " пробіг " + distance + " м. ");
+            return true;
+        }else{
+            System.out.println(name + " не зміг пробігти " + distance + " м. ");
+            return false;
+        }
+    }
+
+    @Override
+    public boolean jump(int height) {
+        if(height <= maxJumpDistance){
+            System.out.println(name + " перестрибнув " + height + " м. ");
+            return true;
+        }else{
+            System.out.println(name + " не зміг перестрибнути" + height + " м. ");
+            return false;
+        }
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
